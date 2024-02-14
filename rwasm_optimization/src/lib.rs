eef7212d63042abe30e4380903cf7d8dca3d09bb
Stas Stepanov <stanislav@ankr.com>
2024-02-14 18:07:18 +0600

    integrated 'run_rwasm_with_raw_input' from fluentbase to test the case

--- rwasm_optimization/src/lib.rs
+++ rwasm_optimization/src/lib.rs
@@ -60,15 +60,62 @@ extern "C" {
 
 #[cfg(target_arch = "wasm32")]
 #[no_mangle]
-fn main() {
-    unsafe {
-        let s = _sys_input_size();
-        assert_ne!(s, 0);
-        let mut v = alloc::vec![0u8; s as usize];
-        _sys_read(v.as_mut_ptr(), 0, s);
-        let r = translate_binary(&v);
-        _sys_write(r.as_ptr(), r.len() as u32);
+pub fn main() {
+    use alloc::string::ToString;
+    use core::{alloc::Layout, ptr};
+    use fluentbase_sdk::{LowLevelAPI, LowLevelSDK};
+    use rwasm_codegen::{
+        rwasm::common::ValueType,
+        Compiler,
+        CompilerConfig,
+        ImportFunc,
+        ImportLinker,
     };
+
+    let size = LowLevelSDK::sys_input_size() as usize;
+    let buffer = unsafe {
+        let buffer = alloc::alloc::alloc(Layout::from_size_align_unchecked(size, 8usize));
+        &mut *ptr::slice_from_raw_parts_mut(buffer, size)
+    };
+    LowLevelSDK::sys_read(buffer, 0);
+    let mut import_linker = ImportLinker::default();
+    import_linker.insert_function(ImportFunc::new_env(
+        "fluentbase_v1alpha".to_string(),
+        "_sys_halt".to_string(),
+        100,
+        &[ValueType::I32],
+        &[],
+        0,
+    ));
+    import_linker.insert_function(ImportFunc::new_env(
+        "fluentbase_v1alpha".to_string(),
+        "_sys_write".to_string(),
+        101,
+        &[ValueType::I32; 2],
+        &[],
+        0,
+    ));
+    import_linker.insert_function(ImportFunc::new_env(
+        "fluentbase_v1alpha".to_string(),
+        "_sys_input_size".to_string(),
+        102,
+        &[],
+        &[ValueType::I32; 1],
+        0,
+    ));
+    import_linker.insert_function(ImportFunc::new_env(
+        "fluentbase_v1alpha".to_string(),
+        "_sys_read".to_string(),
+        103,
+        &[ValueType::I32; 3],
+        &[],
+        0,
+    ));
+    let mut compiler =
+        Compiler::new_with_linker(buffer, CompilerConfig::default(), Some(&import_linker)).unwrap();
+    let rwasm_bytecode = compiler.finalize().unwrap();
+    LowLevelSDK::sys_write(&rwasm_bytecode);
+    LowLevelSDK::sys_halt(0);
 }
 
 #[cfg(test)]
@@ -108,7 +155,7 @@ mod test {
             let module = Module::new(&engine, wasm_binary.as_slice()).unwrap();
             let ctx = RuntimeContext::<()>::new(vec![])
                 .with_state(STATE_MAIN)
-                .with_fuel_limit(1_000_000)
+                .with_fuel_limit(100_000_000)
                 .with_input(input_data.to_vec())
                 .with_catch_trap(true);
             let mut store = Store::new(&engine, ctx);
@@ -151,7 +198,7 @@ mod test {
         let rwasm_binary = wasm2rwasm(wasm_binary.as_slice(), false);
         let ctx = RuntimeContext::new(rwasm_binary)
             .with_state(STATE_MAIN)
-            .with_fuel_limit(1_000_000)
+            .with_fuel_limit(100_000_000)
             .with_input(input_data.to_vec())
             .with_catch_trap(true);
         let import_linker = Runtime::<()>::new_sovereign_linker();
