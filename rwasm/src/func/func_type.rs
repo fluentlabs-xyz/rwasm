76946aff11baf7c9b0c01960ed3f4d523b68de25
Dmitry Savonin <nwtsoas@gmail.com>
2024-04-30 13:47:55 +0400

    removed redundant debug lines

--- rwasm/src/func/func_type.rs
+++ rwasm/src/func/func_type.rs
@@ -127,7 +127,6 @@ impl FuncType {
         T: Ty,
     {
         if self.results().len() != results.len() {
-            let (a, b) = self.params_results();
             return Err(FuncError::MismatchingResultLen);
         }
         if check_type
