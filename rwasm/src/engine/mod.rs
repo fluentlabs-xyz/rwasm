3716bc9c281f70d401ac8972c908b4d37c7043f8
Dmitry Savonin <nwtsoas@gmail.com>
2025-01-09 14:37:44 +0300

    feat(rwasm): expose `FuelCosts` in config module

--- rwasm/src/engine/mod.rs
+++ rwasm/src/engine/mod.rs
@@ -20,7 +20,7 @@ mod tracer;
 pub use self::{
     bytecode::DropKeep,
     code_map::CompiledFunc,
-    config::{Config, FuelConsumptionMode, RwasmConfig, StateRouterConfig},
+    config::{Config, FuelConsumptionMode, FuelCosts, RwasmConfig, StateRouterConfig},
     func_builder::{
         FuncBuilder,
         FuncTranslatorAllocations,
