49e5e692e29a52a9939d3b6d5d190dc01255f725
Dmitry Savonin <nwtsoas@gmail.com>
2025-07-31 16:54:56 +0200

    feat(gas-injector): fix running unit tests

--- gas-injector/src/lib.rs
+++ gas-injector/src/lib.rs
@@ -111,6 +111,9 @@ impl<T: CostModel> GasInjector<T> {
             Payload::CodeSectionEntry(function_body) => {
                 self.process_code_section_entry(function_body)?
             }
+            Payload::CustomSection(_) => {
+                // ignore custom sections
+            }
             _ => self.wasm_output.extend(&data[..consumed_bytes]),
         }
         Ok(())
@@ -360,66 +363,11 @@ impl CostModel for DefaultCostModel {
     fn cost_for(&self, op: &Operator) -> u64 {
         use Operator::*;
         match op {
-            // Memory operations
-            MemoryGrow { .. }
-            | MemoryInit { .. }
-            | MemoryCopy { .. }
-            | MemoryFill { .. }
-            | MemorySize { .. } => MEMORY_BYTES_PER_FUEL as u64,
-            // Table operations
-            TableInit { .. }
-            | TableCopy { .. }
-            | TableFill { .. }
-            | TableGet { .. }
-            | TableSet { .. }
-            | TableGrow { .. }
-            | TableSize { .. } => TABLE_ELEMS_PER_FUEL as u64,
-            // Load/store operations
-            I32Load { .. }
-            | I64Load { .. }
-            | F32Load { .. }
-            | F64Load { .. }
-            | I32Load8S { .. }
-            | I32Load8U { .. }
-            | I32Load16S { .. }
-            | I32Load16U { .. }
-            | I64Load8S { .. }
-            | I64Load8U { .. }
-            | I64Load16S { .. }
-            | I64Load16U { .. }
-            | I64Load32S { .. }
-            | I64Load32U { .. } => LOAD_FUEL_COST as u64,
-            I32Store { .. }
-            | I64Store { .. }
-            | F32Store { .. }
-            | F64Store { .. }
-            | I32Store8 { .. }
-            | I32Store16 { .. }
-            | I64Store8 { .. }
-            | I64Store16 { .. }
-            | I64Store32 { .. } => STORE_FUEL_COST as u64,
-            // Call operations
-            Call { .. } | CallIndirect { .. } => CALL_FUEL_COST as u64,
-            // Control flow, entity ops
-            Br { .. } | BrIf { .. } | BrTable { .. } | Return { .. } => ENTITY_FUEL_COST as u64,
-            // Locals (get/set/tee)
-            LocalGet { .. } | LocalSet { .. } | LocalTee { .. } => LOCALS_PER_FUEL as u64,
-            // Drop/keep (drop, select)
-            Drop | Select => DROP_KEEP_PER_FUEL as u64,
-
-            //TODO: Compare rwasm and wasmtime opcode cost wasm time op code cost
-            // // Nop and drop generate no code, so don't consume fuel for them.
-            // Operator::Nop => 0,
-            //
-            // // Control flow may create branches, but is generally cheap and
-            // // free, so don't consume fuel. Note the lack of `if` since some
-            // // cost is incurred with the conditional check.
-            // Operator::Block { .. }
-            // | Operator::Loop { .. }
-            // | Operator::Unreachable
-            // | Operator::Return
-            // | Operator::Else
-            // | Operator::End => 0,
+            // Control flow may create branches, but is generally inexpensive and
+            // free, so don't consume fuel.
+            // Note the lack of `if` since some
+            // cost is incurred with the conditional check.
+            Block { .. } | Loop { .. } | Unreachable | Return | Else | End => 0,
 
             // Most other ops cost base
             _ => BASE_FUEL_COST as u64,
@@ -515,10 +463,13 @@ mod tests {
             new_wat,
             r#"(module
   (type (;0;) (func (param i32) (result i32)))
-  (type (;1;) (func))
-  (export "f" (func $f))
-  (func $f (;0;) (type 0) (param i32) (result i32)
+  (type (;1;) (func (param i32)))
+  (import "env" "_charge_gas" (func (;0;) (type 1)))
+  (export "f" (func 1))
+  (func (;1;) (type 0) (param i32) (result i32)
     local.get 0
+    i32.const 1
+    call 0
   )
 )
 "#
@@ -530,7 +481,7 @@ mod tests {
         let new_wat = test_type_injection(
             r#"(module
   (type (;0;) (func (param i32) (result i32)))
-  (type (;1;) (func))
+  (type (;1;) (func (param i32)))
   (export "f" (func $f))
   (func $f (;0;) (type 0) (param i32) (result i32)
     local.get 0
@@ -542,10 +493,13 @@ mod tests {
             new_wat,
             r#"(module
   (type (;0;) (func (param i32) (result i32)))
-  (type (;1;) (func))
-  (export "f" (func $f))
-  (func $f (;0;) (type 0) (param i32) (result i32)
+  (type (;1;) (func (param i32)))
+  (import "env" "_charge_gas" (func (;0;) (type 1)))
+  (export "f" (func 1))
+  (func (;1;) (type 0) (param i32) (result i32)
     local.get 0
+    i32.const 1
+    call 0
   )
 )
 "#
@@ -570,13 +524,18 @@ mod tests {
             r#"(module
   (type (;0;) (func (param i32) (result i32)))
   (type (;1;) (func (param i64) (result f32)))
-  (type (;2;) (func))
-  (export "g" (func $g))
-  (func (;0;) (type 0) (param i32) (result i32)
+  (type (;2;) (func (param i32)))
+  (import "env" "_charge_gas" (func (;0;) (type 2)))
+  (export "g" (func 2))
+  (func (;1;) (type 0) (param i32) (result i32)
     local.get 0
+    i32.const 1
+    call 0
   )
-  (func $g (;1;) (type 1) (param i64) (result f32)
+  (func (;2;) (type 1) (param i64) (result f32)
     f32.const 0x1p+0 (;=1;)
+    i32.const 1
+    call 0
   )
 )
 "#
