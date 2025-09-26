22a593e849d62c62fc0f5e1724d5294daf60edd2
bfdays <stanislav@fluentlabs.xyz>
2025-09-26 22:21:57 +0700

    feat(segment-caching): fix bug, extend unit-test

--- src/types/bitvec_inlined.rs
+++ src/types/bitvec_inlined.rs
@@ -3,20 +3,23 @@ use bitvec::order::Lsb0;
 use bitvec::store::BitStore;
 use bitvec::vec::BitVec;
 use core::cmp::min;
+use std::ops::Range;
 
 pub const USIZE_BITS: usize = 0_usize.count_zeros() as usize;
 
+pub type ElemType = usize;
+
 pub struct BitVecInlined<const INLINE: usize> {
-    pub inline_data: [usize; INLINE],
+    pub inline_data: [ElemType; INLINE],
     pub inline_data_bit_len: usize,
-    pub bit_vec: BitVec<usize, Lsb0>,
+    pub bit_vec: BitVec<ElemType, Lsb0>,
 }
 
 impl<const INLINE: usize> BitVecInlined<INLINE> {
     pub const INLINE_DATA_BIT_LEN_MAX: usize = INLINE * USIZE_BITS;
 
     pub fn new(bit_vec: BitVec) -> Self {
-        let static_vec = [usize::MIN; INLINE];
+        let static_vec = [ElemType::MIN; INLINE];
         let static_len = if bit_vec.len() > INLINE {
             INLINE
         } else {
@@ -45,27 +48,31 @@ impl<const INLINE: usize> BitVecInlined<INLINE> {
 
 impl<const INLINE: usize> BitVecInlined<INLINE> {
     pub const EMPTY: Self = Self {
-        inline_data: [usize::MIN; INLINE],
+        inline_data: [ElemType::MIN; INLINE],
         inline_data_bit_len: 0,
         bit_vec: BitVec::<_, _>::EMPTY,
     };
 
-    #[inline]
-    pub fn repeat(bit: bool, len: usize) -> Self {
-        let mut inline_data = [usize::MIN; INLINE];
-        let mut idx = 0;
-        let inline_data_bit_len = min(len, Self::INLINE_DATA_BIT_LEN_MAX);
-        while idx < inline_data_bit_len {
+    fn fill_range(data: &mut [ElemType; INLINE], range: Range<usize>, value: bool) {
+        let mut idx = range.start;
+        while idx < range.end {
             let (item_idx, item_shift_idx_base) = Self::relative_indexes(idx);
-            let item = &mut inline_data[item_idx];
-            let mut item_shift_idx = item_shift_idx_base;
-            while item_shift_idx < Self::INLINE_DATA_BIT_LEN_MAX {
-                Self::set_bit(item, item_shift_idx, bit);
-                item_shift_idx += 1;
+            let item = &mut data[item_idx];
+            let mut shift_idx = item_shift_idx_base;
+            while shift_idx < Self::INLINE_DATA_BIT_LEN_MAX {
+                Self::set_bit(item, shift_idx, value);
+                shift_idx += 1;
             }
 
-            idx += item_shift_idx - item_shift_idx_base;
+            idx += shift_idx - item_shift_idx_base;
         }
+    }
+
+    #[inline]
+    pub fn repeat(bit: bool, len: usize) -> Self {
+        let mut inline_data = [ElemType::MIN; INLINE];
+        let inline_data_bit_len = min(len, Self::INLINE_DATA_BIT_LEN_MAX);
+        Self::fill_range(&mut inline_data, 0..inline_data_bit_len, bit);
         let bit_vec = if len <= Self::INLINE_DATA_BIT_LEN_MAX {
             BitVec::<_, _>::EMPTY
         } else {
@@ -84,7 +91,7 @@ impl<const INLINE: usize> BitVecInlined<INLINE> {
     }
 
     pub fn fill(&mut self, value: bool) {
-        let fill = if value { usize::MAX } else { usize::MIN };
+        let fill = if value { ElemType::MAX } else { ElemType::MIN };
         self.inline_data.fill(fill);
         if !self.bit_vec.is_empty() {
             self.bit_vec.fill(value);
@@ -123,8 +130,16 @@ impl<const INLINE: usize> BitVecInlined<INLINE> {
     #[inline]
     pub fn resize(&mut self, new_len: usize, value: bool) {
         if self.inline_data_bit_len < new_len {
-            self.inline_data_bit_len = min(new_len, Self::INLINE_DATA_BIT_LEN_MAX);
-            // TODO fill new bits
+            let new_inline_data_bit_len = min(new_len, Self::INLINE_DATA_BIT_LEN_MAX);
+            Self::fill_range(
+                &mut self.inline_data,
+                self.inline_data_bit_len..new_inline_data_bit_len,
+                value,
+            );
+            let len = self.inline_data_bit_len;
+            while len < new_inline_data_bit_len {}
+
+            self.inline_data_bit_len = new_inline_data_bit_len;
         }
         if new_len > Self::INLINE_DATA_BIT_LEN_MAX {
             let dynamic_len = new_len - Self::INLINE_DATA_BIT_LEN_MAX;
@@ -177,15 +192,20 @@ mod tests {
 
     #[test]
     fn bit_vec_inlined() {
-        let len = 65;
+        let mut len = 65;
         let mut idx = 1;
         assert!(idx < len);
         let mut value = true;
         let mut bv = BitVecInlined::<1>::repeat(value, len);
-        assert_eq!(bv.get(idx).unwrap(), value);
+        assert_eq!(bv.get(idx), Some(value));
         idx = 64;
         value = false;
         bv.set(idx, value);
-        assert_eq!(bv.get(idx).unwrap(), value);
+        assert_eq!(bv.get(idx), Some(value));
+        assert_eq!(bv.get(len), None);
+        len += 1;
+        value = true;
+        bv.resize(len, value);
+        assert_eq!(bv.get(len - 1), Some(value));
     }
 }
