2cf0af595e7272e7cae381834e5c401a3039bf93
Dmitry Savonin <nwtsoas@gmail.com>
2025-10-03 21:42:29 +0300

    chore: add evm benchmarks

--- benchmarks/lib.rs
+++ benchmarks/lib.rs
@@ -1,6 +1,6 @@
 #[cfg(target_arch = "wasm32")]
 #[no_mangle]
-pub fn main(n: i32) -> i32 {
+pub fn main(n: u32) -> u32 {
     let (mut a, mut b) = (0, 1);
     for _ in 0..n {
         let temp = a;
