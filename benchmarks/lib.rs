6ba5bf41ff82f0b30ad454d24b20192e16c4512f
Stanislav Stepanov <stanislav@fluentlabs.xyz>
2025-10-06 20:46:10 +0700

    feat(rwasm): added fib32 test as regular test for trials

--- benchmarks/lib.rs
+++ benchmarks/lib.rs
@@ -32,3 +32,39 @@ pub fn fib256(n: u64) -> U256 {
     }
     a
 }
+
+#[cfg(test)]
+mod tests {
+    use rwasm::{
+        always_failing_syscall_handler, CompilationConfig, ExecutionEngine, FuelConfig,
+        ImportLinker, RwasmModule, Strategy, Value,
+    };
+    use std::sync::Arc;
+
+    const FIB_VALUE: i32 = 41;
+
+    #[test]
+    fn fib32_test() {
+        let wasm_binary = include_bytes!("./lib.wasm");
+        let config = CompilationConfig::default()
+            .with_entrypoint_name("fib32".into())
+            .with_allow_malformed_entrypoint_func_type(true)
+            .with_consume_fuel(false);
+        let (module, _) = RwasmModule::compile(config, wasm_binary).unwrap();
+        let strategy = Strategy::Rwasm {
+            module: module.clone(),
+            engine: ExecutionEngine::acquire_shared(),
+        };
+        let mut store = strategy.create_store(
+            Arc::new(ImportLinker::default()),
+            (),
+            always_failing_syscall_handler,
+            FuelConfig::default(),
+        );
+        let mut result = [Value::I32(0)];
+        strategy
+            .execute(&mut store, "fib32", &[Value::I32(FIB_VALUE)], &mut result)
+            .unwrap();
+        core::hint::black_box(result);
+    }
+}
