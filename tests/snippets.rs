599da3de86d350960c80ec96bc99988c59202abe
d1r1 <me@d1r1.me>
2025-10-23 17:09:27 +0400

    fix(tests): replace ExecutionEngine::new() with ::default()

--- tests/snippets.rs
+++ tests/snippets.rs
@@ -1237,7 +1237,7 @@ fn run_i64_binary_op(op: &str, a: i64, b: i64, expected: i64) {
     let (rwasm_module, _) = RwasmModule::compile(config, &wasm_binary).unwrap();
 
     let mut store = RwasmStore::<()>::default();
-    let engine = ExecutionEngine::new();
+    let engine = ExecutionEngine::default();
     let mut result = [Value::I64(0); 1];
 
     let execution_result = engine.execute(
@@ -1288,7 +1288,7 @@ fn run_i64_comparation_op(op: &str, a: i64, b: i64, expected: bool) {
     let (rwasm_module, _) = RwasmModule::compile(config, &wasm_binary).unwrap();
 
     let mut store = RwasmStore::<()>::default();
-    let engine = ExecutionEngine::new();
+    let engine = ExecutionEngine::default();
     let mut result = [Value::I32(0); 1];
 
     let execution_result = engine.execute(
