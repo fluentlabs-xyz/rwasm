62d01d1d5ed55794e79dc3e1ace8434bebe1fd4e
Stanislav Stepanov <truebfday@gmail.com>
2026-01-23 15:43:06 +0400

    bug(compile-time-ExternRef-not-supported-as-a-local): ExternRef type not supported as a local during compilation (#101)

--- tests/compiler.rs
+++ tests/compiler.rs
@@ -1,5 +1,10 @@
-use rwasm::{CompilationConfig, RwasmModule};
+use rwasm::{CompilationConfig, CompilationError, ConstructorParams, RwasmModule};
 
+fn test_compilation(wat_str: &str) -> Result<(RwasmModule, ConstructorParams), CompilationError> {
+    let wasm = wat::parse_str(wat_str).expect("valid WAT");
+    let config = CompilationConfig::default();
+    RwasmModule::compile(config, &wasm)
+}
 #[test]
 fn test_i64_type_split_into_2_x_i32_bug() {
     const WAT: &str = r#"
@@ -22,9 +27,28 @@ fn test_i64_type_split_into_2_x_i32_bug() {
             end)
           (export "!" (func 0)))
     "#;
-    let wasm = wat::parse_str(WAT).expect("valid WAT");
-
-    let config = CompilationConfig::default();
-
-    let _ = RwasmModule::compile(config, &wasm);
+    let _ = test_compilation(WAT);
+}
+#[test]
+fn test_extern_ref_not_supported_as_a_local_bug() {
+    const WAT: &str = r#"
+        (module
+          (type (;0;) (func))
+          (global (;0;) (mut i32) i32.const 1000)
+          (export "" (func 0))
+          (func (;0;) (type 0)
+            (local f64 externref)
+            global.get 0
+            i32.eqz
+            if ;; label = @1
+              unreachable
+            end
+            global.get 0
+            i32.const 1
+            i32.sub
+            global.set 0
+          )
+        (export "!" (func 0)))
+    "#;
+    let _ = test_compilation(WAT);
 }
