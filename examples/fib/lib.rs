8c4ecfe9f2dbdffe57de9d49c15dce8d4f04b311
Dmitry Savonin <dmitry@fluentlabs.xyz>
2026-02-20 15:24:53 +0400

    fix: failing build for fib

--- examples/fib/lib.rs
+++ examples/fib/lib.rs
@@ -1,1 +1,2 @@
-pub const FIB_WASM: &[u8] = include_bytes!("./target/wasm32-unknown-unknown/release/fib.wasm");
+#[cfg(not(target_arch = "wasm32"))]
+pub const FIB_WASM: &[u8] = include_bytes!(env!("OUTPUT_WASM_PATH"));
